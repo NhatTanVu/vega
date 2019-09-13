530c3a2aa596011e97700e943611d23279cd8a65
NhatTanVu <onchua2006@gmail.com>
2019-09-14 07:47:54 +0800

    Allow to run Angular CLI Server on both Development and Production mode. Add ng2-toasty(display toast notifcation), underscore(add some array utilities) and sentry(log and monitor error). Bind and submit create/edit/delete Vehicle form. Implement client-side validation, parallel requests, interfaces.

--- ClientApp/src/app/services/vehicle.service.ts
+++ ClientApp/src/app/services/vehicle.service.ts
@@ -1,3 +1,4 @@
+import { SaveVehicle } from './../models/vehicle';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { map } from 'rxjs/operators';
@@ -17,5 +18,25 @@ export class VehicleService {
   getFeatures() {
     return this.http.get('/api/features')
     .pipe(map(res => res.json()));
+  }
+
+  create(vehicle) {
+    return this.http.post('/api/vehicles', vehicle)
+    .pipe(map(res => res.json()));
+  }
+
+  getVehicle(id) {
+    return this.http.get('/api/vehicles/' + id)
+    .pipe(map(res => res.json()));
+  }
+
+  update(vehicle: SaveVehicle) {
+    return this.http.put('/api/vehicles/' + vehicle.id, vehicle)
+    .pipe(map(res => res.json()));
+  }
+
+  delete(id) {
+    return this.http.delete('/api/vehicles/' + id)
+    .pipe(map(res => res.json()));    
   }
 }
